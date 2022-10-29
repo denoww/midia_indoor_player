8d642d24056d7a77a5df7a711787f4dde8a851cf
denoww <denoww@gmail.com>
2022-10-29 15:04:44 -0300

    Update start_on_machine_boot.js

--- start_on_machine_boot.js
+++ start_on_machine_boot.js
@@ -1,1 +1,20 @@
+// startup on boot (da lib auto-launch)
+var AutoLaunch, autoLauncher;
 
+AutoLaunch = require('auto-launch');
+
+autoLauncher = new AutoLaunch({
+  name: 'midia_indoor_player',
+  path: '/var/lib/midia_indoor_player/tasks/init.sh'
+});
+
+autoLauncher.enable();
+
+autoLauncher.isEnabled().then(function(isEnabled) {
+  if (isEnabled) {
+    return;
+  }
+  autoLauncher.enable();
+}).catch(function(err) {
+  throw err;
+});
