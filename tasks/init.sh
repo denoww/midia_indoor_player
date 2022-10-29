b6227350643ffc55c2ecea7bdffac0ee24b867a8
denoww <denoww@gmail.com>
2022-10-29 14:41:33 -0300

    Update init.sh

--- tasks/init.sh
+++ tasks/init.sh
@@ -8,6 +8,13 @@ cd /var/lib/midia_indoor_player/
 /usr/bin/npm run update_timezone &
 /usr/bin/npm start &
 
+# a cada x minutos verifica se tá rodando, senão chama o reinit.sh
+while [[ true ]]
+do
+  sleep 2m
+  /var/lib/midia_indoor_player/tasks/./reinit.sh
+done
+
 # cliques na tela para simular acao do usuario para corrigir problema
 # de play nos videos
 array=( 200 150 100 50 )
