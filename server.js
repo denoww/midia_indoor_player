570f0c2cffa728e6f89f3f8790f95bc1afc5351d
Rodrigo <denoww@gmail.com>
2023-04-22 14:28:09 -0300

    removido app electron

--- server.js
+++ server.js
@@ -1,9 +1,3 @@
-if (process.versions.electron) {
-  delete process.env.ELECTRON_ENABLE_SECURITY_WARNINGS;
-  process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = true;
-  process.env.ELECTRON_ENABLE_STACK_DUMPING = true;
-}
-
 global.homePath = (process.env.HOME || process.env.USERPROFILE || process.env.HOMEPATH) + '/';
 
 path = require('path');
@@ -14,18 +8,48 @@ require('sc-node-tools');
 require('./app/classes/logs')(true);
 // require('./start_on_machine_boot')
 
-if (process.versions.electron) {
-  require('./application');
-} else {
-  require('./app/classes/commons');
-  require('./app/classes/versions_control')();
-  require('./app/classes/download')();
-  require('./app/classes/grade')();
-  require('./app/classes/feeds')();
-  require('./app/servers/web')();
-  global.grade.startCheckTvTimer()
-}
 
-process.on('uncaughtException', function (error) {
-  logs.error("UncaughtException: " + error);
-});
+require('./app/classes/commons');
+require('./app/classes/versions_control')();
+require('./app/classes/download')();
+require('./app/classes/grade')();
+require('./app/classes/feeds')();
+require('./app/servers/web')();
+global.grade.startCheckTvTimer()
+
+
+
+
+
+
+// if (process.versions.electron) {
+//   delete process.env.ELECTRON_ENABLE_SECURITY_WARNINGS;
+//   process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = true;
+//   process.env.ELECTRON_ENABLE_STACK_DUMPING = true;
+// }
+
+// global.homePath = (process.env.HOME || process.env.USERPROFILE || process.env.HOMEPATH) + '/';
+
+// path = require('path');
+
+// require('coffeescript').register();
+// require('./env');
+// require('sc-node-tools');
+// require('./app/classes/logs')(true);
+// // require('./start_on_machine_boot')
+
+// if (process.versions.electron) {
+//   require('./application');
+// } else {
+//   require('./app/classes/commons');
+//   require('./app/classes/versions_control')();
+//   require('./app/classes/download')();
+//   require('./app/classes/grade')();
+//   require('./app/classes/feeds')();
+//   require('./app/servers/web')();
+//   global.grade.startCheckTvTimer()
+// }
+
+// process.on('uncaughtException', function (error) {
+//   logs.error("UncaughtException: " + error);
+// });
